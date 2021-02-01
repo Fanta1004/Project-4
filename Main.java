2bc6403fbdb426dfed1eca79b0a069dff8395ea8
Jared Doval <jared.doval@patelhs.org>
2021-02-01 14:11:37 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+
+  public static void main(String[] args) 
+  {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("What's your favorite food?");
+    int favoriteFood =
+    //jared doval
+//Project 4 2-1-2021
