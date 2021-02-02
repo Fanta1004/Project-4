a4a10629386015ddb96c1ba594a59e059494f88e
Jared Doval <jared.doval@patelhs.org>
2021-02-02 18:59:21 +0000

    Added comments and structured code

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
-
+ //jared doval
+//Project 4 2-1-2021
 class Main {
 
   public static void main(String[] args) 
@@ -7,5 +8,4 @@ class Main {
     Scanner scan = new Scanner(System.in);
     System.out.println("What's your favorite food?");
     int favoriteFood =
-    //jared doval
-//Project 4 2-1-2021
+   
